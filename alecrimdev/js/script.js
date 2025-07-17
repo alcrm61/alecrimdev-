082206de7673c840be11cdd345db34717a034881
Isabela Alecrim <belinhalecrim33@gmail.com>
2025-07-17 18:32:15 -0300

    implementacao

--- alecrimdev/js/script.js
+++ alecrimdev/js/script.js
@@ -57,3 +57,31 @@ const typed = new Typed ('.multiple-text', {
     backDelay: 1000,
     loop: true
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const swiper = new Swiper(".portfolio-container", {
+      loop: true,
+      grabCursor: true,
+      spaceBetween: 30,
+      slidesPerView: 1,
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+      },
+    });
+  });
